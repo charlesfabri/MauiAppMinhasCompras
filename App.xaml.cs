9208822dc71dabe3e9d814d5839f7e2475290fdf
charlesfabri <charles_fabri@hotmail.com>
2025-06-09 15:31:25 -0300

    Add Models e Troca da Tela Inicial

--- App.xaml.cs
+++ App.xaml.cs
@@ -6,7 +6,13 @@
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            // MainPage = new AppShell();
+
+            //- Define a MainPage do aplicativo como uma página de navegação contendo ListaProduto (uma outra view do app).
+            //- Troca de Tela Inciial
+            MainPage = new NavigationPage(new Views.ListaProduto());                  
         }
     }
 }
+
+
